ad37637a495101f502d6619b3ad10daa3a9e699d
htense <hortenserothery@gmail.com>
2020-10-06 17:10:47 +1300

    add totals

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -23,18 +25,32 @@ function makeGreen (evt) {
 
 // CREATE FUNCTION makeBlue HERE
 
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
+
 // CREATE FUNCTION hide HERE
+
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
-    blue: 0,
-    green: 0,
-    invisible: 0
+    blue: 2,
+    green: 0,  
+    invisible: 1
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
+
+
+;
   // Once you've done the counting, this function will update the display
+
   displayTotals(totals)
 }
 
